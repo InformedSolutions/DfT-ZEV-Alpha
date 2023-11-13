eb5c961c36410c29a4135c4545bf5f66991e6ea6
Kacper S <kacpersmal@gmail.com>
2023-11-13 07:53:04 +0100

    Update Function.cs

--- src/Zev/Zev.Services.ComplianceCalculationService.Handler/Function.cs
+++ src/Zev/Zev.Services.ComplianceCalculationService.Handler/Function.cs
@@ -1,8 +1,12 @@
+using System;
+using System.Text.Json;
 using Google.Cloud.Functions.Framework;
 using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
 using Google.Cloud.Functions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Zev.Core.Infrastructure.Configuration;
 using Zev.Core.Infrastructure.Persistence;
 
 namespace Zev.Services.ComplianceCalculationService.Handler;
@@ -12,26 +16,34 @@ public class Function : IHttpFunction
 {
     private readonly ILogger<Function> _logger;
     private readonly AppDbContext _context;
-
-    public Function(AppDbContext context, ILogger<Function> logger)
+    private readonly PostgresConfiguration _postgresConfiguration;
+    public Function(AppDbContext context, ILogger<Function> logger,  IOptions<PostgresConfiguration> postgresConfiguration)
     {
         _context = context;
         _logger = logger;
+        _postgresConfiguration = postgresConfiguration.Value;
     }
 
     public async Task HandleAsync(HttpContext context)
     {
         _logger.LogInformation("Started processing request.");
+        var settingsJson = JsonSerializer.Serialize(_postgresConfiguration);
+        _logger.LogInformation("Postgres settings: {settingsJson}", settingsJson);
         
-        var canConnect = await _context.Database.CanConnectAsync();
-
-        if (canConnect)
+        try
         {
-            _logger.LogInformation("Successfully connected to database.");
-        }
-        else
+            var canConnect = await _context.Database.CanConnectAsync();
+            if (canConnect)
+            {
+                _logger.LogInformation("Successfully connected to database.");
+            }
+            else
+            {
+                _logger.LogCritical("Failed to connect to database.");
+            }
+        } catch (Exception e)
         {
-            _logger.LogCritical("Failed to connect to database.");
+            _logger.LogError(e, "Error while connecting to db.");
         }
         
         await context.Response.WriteAsync($"Hello!");
