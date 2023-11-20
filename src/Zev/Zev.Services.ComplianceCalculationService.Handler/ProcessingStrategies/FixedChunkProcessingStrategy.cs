7154fb31ecd2945be8122f386a65cf9c254b2462
Kacper S <kacpersmal@gmail.com>
2023-11-20 10:36:50 +0100

    Cleanup FixedChunkProcessingStrategy

--- src/Zev/Zev.Services.ComplianceCalculationService.Handler/ProcessingStrategies/FixedChunkProcessingStrategy.cs
+++ src/Zev/Zev.Services.ComplianceCalculationService.Handler/ProcessingStrategies/FixedChunkProcessingStrategy.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using CsvHelper;
@@ -17,84 +16,99 @@ using Zev.Services.ComplianceCalculationService.Handler.Maps;
 
 namespace Zev.Services.ComplianceCalculationService.Handler.ProcessingStrategies;
 
+/// <summary>
+/// Implements the IProcessingStrategy interface to provide a strategy for processing data in fixed chunks.
+/// </summary>
 public class FixedChunkProcessingStrategy : IProcessingStrategy
 {
-    //DI
     private readonly ILogger _logger;
     private readonly IUnitOfWork _unitOfWork;
     private readonly IMapper _mapper;
 
-    //Fields
-    private readonly Stopwatch _stopwatch;
-    private readonly ConcurrentStack<RawVehicleDTO> _bufferStack;
-    private int _recordCounter;
-    private int _bufferCounter;
+    private readonly Stopwatch _stopwatch = new Stopwatch();
+    private readonly ConcurrentStack<RawVehicleDTO> _bufferStack = new ConcurrentStack<RawVehicleDTO>();
+
+    private int _recordCounter = 0;
+    private int _bufferCounter = 0;
+
     public FixedChunkProcessingStrategy(ILogger logger, IUnitOfWork unitOfWork, IMapper mapper)
     {
         _logger = logger;
         _unitOfWork = unitOfWork;
         _mapper = mapper;
-        _stopwatch = new Stopwatch();
-        _bufferStack = new ConcurrentStack<RawVehicleDTO>();
-        _recordCounter = 0;
-        _bufferCounter = 0;
     }
+
+    /// <summary>
+    /// Processes the given stream in fixed-size chunks.
+    /// </summary>
+    /// <param name="stream">The stream to process.</param>
+    /// <param name="chunkSize">The size of the chunks to process.</param>
+    /// <returns>A ProcessingResult indicating the result of the processing.</returns>
     public async Task<ProcessingResult> ProcessAsync(Stream stream, int chunkSize)
     {
+        _logger.Information("Processing started.");
         _stopwatch.Start();
 
         using var reader = new StreamReader(stream);
         await using var transaction = await _unitOfWork.BeginTransactionAsync();
+
         _logger.Information("Beginning transaction: {TransactionId}", transaction.TransactionId);
+
         try
         {
-            using (var csv = new CsvReader(reader, GetCsvConfig()))
-            {
-                csv.Context.RegisterClassMap<RawVehicleCsvMap>();
-                while (await csv.ReadAsync())
-                {
-                    var record = csv.GetRecord<RawVehicleDTO>();
-                    _bufferStack.Push(record);
+            await ReadFromCsvAndProcessBuffer(reader, chunkSize);
 
-                    if (_bufferStack.Count >= chunkSize)
-                    {
-                        await ProcessBuffer();
-                    }
-                }
+            // Process the remaining records
+            if (!_bufferStack.IsEmpty)
+            {
+                _logger.Information("Processing remaining records in buffer.");
+                await ProcessBuffer();
             }
 
-            //Process the remaining records
-            if (!_bufferStack.IsEmpty)
-                await ProcessBuffer();
-            
+            _logger.Information("Committing transaction.");
             await transaction.CommitAsync();
         }
         catch (Exception ex)
         {
+            _logger.Error(ex, "Error processing file. Rolling back transaction.");
             await transaction.RollbackAsync();
-            _logger.Error(ex, "Error processing file");
             return ProcessingResult.Fail(_recordCounter, _stopwatch.ElapsedMilliseconds, _bufferCounter);
         }
 
-        _stopwatch.Stop();
-        
+        _logger.Information("Processing completed. Processed {RecordCounter} records in {ElapsedMilliseconds} milliseconds.", _recordCounter, _stopwatch.ElapsedMilliseconds);
         return ProcessingResult.Successful(_recordCounter, _stopwatch.ElapsedMilliseconds, _bufferCounter);
+    }
+
+    private async Task ReadFromCsvAndProcessBuffer(StreamReader reader, int chunkSize)
+    {
+        using var csv = new CsvReader(reader, GetCsvConfig());
+        csv.Context.RegisterClassMap<RawVehicleCsvMap>();
+
+        while (await csv.ReadAsync())
+        {
+            var record = csv.GetRecord<RawVehicleDTO>();
+            _bufferStack.Push(record);
+
+            if (_bufferStack.Count >= chunkSize)
+            {
+                await ProcessBuffer();
+            }
+        }
     }
 
     private async Task ProcessBuffer()
     {
         var stackCount = _bufferStack.Count;
-        //_logger.Information("Processing buffer {BufferCounter} with {StackCount} records", _bufferCounter, stackCount);
+        _logger.Information("Processing buffer {BufferCounter} with {StackCount} records", _bufferCounter, stackCount);
         var mappedVehicles = _mapper.Map<IEnumerable<Vehicle>>(_bufferStack);
-        
-        //await _context.AddRangeAsync(mappedVehicles); 
+
         await _unitOfWork.Vehicles.BulkInsertAsync(mappedVehicles);
-        
+
         _recordCounter += stackCount;
         _bufferCounter++;
         _bufferStack.Clear();
     }
-    
+
     private static CsvConfiguration GetCsvConfig() =>
         new(CultureInfo.InvariantCulture)
         {
