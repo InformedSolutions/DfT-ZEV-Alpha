b5925e7592671a5d68d1dc40bb4a76d456831bf8
Kacper S <kacpersmal@gmail.com>
2023-11-21 14:33:27 +0100

    Add gcp sink to serilog

---
+++ src/Zev/Zev.Core.Infrastructure/Configuration/GoogleCloudConfiguration.cs
@@ -0,0 +1,8 @@
+namespace Zev.Core.Infrastructure.Configuration;
+
+public class GoogleCloudConfiguration
+{
+    public const string SectionName = "GoogleCloud";
+    
+    public string ProjectId { get; set; }
+}
