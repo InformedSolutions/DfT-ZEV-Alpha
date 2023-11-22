8011d2e5dddf813db7d0afe3889ee59e727b44a8
Kacper S <kacpersmal@gmail.com>
2023-11-22 21:04:12 +0100

    Process Migration

---
+++ src/Zev/Zev.Core.Infrastructure/Migrations/20231122200212_ProcessMigrations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Zev.Core.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProcessMigrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Processes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    State = table.Column<int>(type: "integer", nullable: false),
+                    Metadata = table.Column<JsonDocument>(type: "jsonb", nullable: true),
+                    Result = table.Column<JsonDocument>(type: "jsonb", nullable: true),
+                    Created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    LastUpdated = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Started = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Finished = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Processes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Processes");
+        }
+    }
+}
