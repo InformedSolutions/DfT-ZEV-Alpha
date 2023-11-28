4143adaa89f685e5a674eadac3baffbbaac686ce
Proctor <proctorj@informed.co.uk>
2023-11-28 12:10:43 +0000

    Updated field names

--- end-to-end-tests/compliance-calculation-performance/csv_generator.py
+++ end-to-end-tests/compliance-calculation-performance/csv_generator.py
@@ -12,56 +12,58 @@ number_of_records = int(argv[2])
 
 type_approvals = ['N1', 'N2', 'M1', 'M2']
 rlce = ['f0','f1','f2']
-country = ['GB-SCT', 'GB-WLS', 'GB-CYM', 'GB-ENG']
+country = ['GB', 'NI']
 fuels = ['Petrol', 'Diesel', 'LPG', 'Electric', 'Petrol-Hybrid']
 
 with open(output_filename, 'w', newline='') as file:
     wr = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
     # add headers
-    wr.writerow(["vin", "vfn", "Mh", "Man", "MMS", "TAN", "T", "Va", "Ve", "Mk", "Cn", "Ct", "Cr", "M", "MT", "MRVL", "Ewltp", "TPMLM", "W",
-                  "At1", "At2", "Ft", "Fm", "Ec", "Z", "IT", "Erwltp", "ber", "dofr", "scheme_year", "postcode", "spvc", "wrm", "mnp", "rlce", "fa", "trrc",
-                  "registered_in_nation"])
+    wr.writerow(["vin", "vfn", "eu_manufacturer_name", "uk_registry_manufacturer_name", "oem_manufacturer_name", "type_approval_number", "type", "variant", "version", "make", "model", 
+                 "type_approval_category", "registered_category", "mass_in_running_order", "wltp_test_mass", "mass_representitive_of_vehicle_load", "monitoring_mass", 
+                 "wltp_c02_emissions", "technically_permissible_maximum_laden_mass", "wheelbase", "axle_track_1", "axle_track_2", "fuel_type", "fuel_mix",
+                 "engine_capacity", "electric_energy_consuption", "eco_innovations", "eco_emissions_reduction", "battery_electric_range", "date_of_first_registration",
+                 "scheme_year", "postcode", "special_purpose_vehicle_category", "road_load_coefficient_f0", "road_load_coefficient_f1", 
+                 "road_load_coefficient_f2", "frontal_area", "tyre_rolling_resistance_class", "registration_location"])
 
     # generate faked vehicle data
     for i in range(number_of_records):
         wr.writerow([
             fake.vin(),                                     # vin
             fake.random_number(digits=25,fix_len=True),     # vfn
-            fake.company(),                                 # Mh
-            fake.vehicle_make(),                            # Man
-            fake.company(),                                 # MMS
-            fake.bothify(text='?##*####/####*####*##'),     # TAN
-            fake.vehicle_category(),                        # T
-            fake.bothify(text='???????????'),               # Va
-            fake.bothify(text='???????????'),               # Ve
-            fake.vehicle_model(),                           # Mk
-            fake.company(),                                 # Cn
-            random.choice(type_approvals),                  # Ct
-            fake.bothify(text='???????????'),               # Cr
-            fake.random_number(digits=4,fix_len=True),      # M
-            fake.random_number(digits=4,fix_len=True),      # MT
-            fake.numerify(text='#%%'),                      # MRVL
-            fake.numerify(text='#%%'),                      # Ewltp
-            fake.random_number(digits=4,fix_len=True),      # TPMLM
-            fake.random_number(digits=4,fix_len=True),      # W
-            fake.random_number(digits=3,fix_len=True),      # At1
-            fake.random_number(digits=3,fix_len=True),      # At2
-            random.choice(fuels),                           # Ft
-            fake.lexify(text='?'),                          # Fm
-            fake.random_number(digits=4,fix_len=True),      # Ec
-            fake.random_number(digits=2,fix_len=False),     # Z
-            fake.text(max_nb_chars=200),                    # IT
-            fake.random_number(digits=2,fix_len=False),     # Erwltp
-            fake.numerify(text='#%%'),                      # ber
-            fake.date(),                                    # dofr
+            fake.company(),                                 # eu_manufacturer_name
+            fake.company(),                                 # uk_registry_manufacturer_name
+            fake.numerify(text='#%%'),                      # oem_manufacturer_name
+            fake.bothify(text='?##*####/####*####*##'),     # type_approval_number
+            fake.vehicle_category(),                        # type
+            fake.bothify(text='???????????'),               # variant
+            fake.bothify(text='???????????'),               # version
+            fake.vehicle_model(),                           # make
+            fake.company(),                                 # model
+            random.choice(type_approvals),                  # type_approval_category
+            fake.bothify(text='???????????'),               # registered_category
+            fake.random_number(digits=4,fix_len=True),      # mass_in_running_order
+            fake.random_number(digits=4,fix_len=True),      # wltp_test_mass
+            fake.numerify(text='#%%'),                      # mass_representitive_of_vehicle_load
+            fake.numerify(text='#%%'),                      # monitoring_mass
+            fake.numerify(text='#%%'),                      # wltp_c02_emissions
+            fake.random_number(digits=4,fix_len=True),      # technically_permissible_maximum_laden_mass
+            fake.random_number(digits=4,fix_len=True),      # wheelbase
+            fake.random_number(digits=3,fix_len=True),      # axle_track_1
+            fake.random_number(digits=3,fix_len=True),      # axle_track_2
+            random.choice(fuels),                           # fuel_type
+            fake.lexify(text='?'),                          # fuel_mix
+            fake.random_number(digits=4,fix_len=True),      # engine_capacity
+            fake.random_number(digits=2,fix_len=False),     # electric_energy_consuption
+            fake.text(max_nb_chars=200),                    # eco_innovations
+            fake.random_number(digits=2,fix_len=False),     # eco_emissions_reduction
+            fake.numerify(text='#%%'),                      # battery_electric_range
+            fake.date(),                                    # date_of_first_registration
             2023,                                           # scheme_year
             fake.postcode(),                                # postcode
-            fake.bothify(text='???????????'),               # spvc
-            fake.boolean(chance_of_getting_true=95),        # wrm
-            fake.numerify(text='#%%'),                      # mnp
+            fake.bothify(text='???????????'),               # special_purpose_vehicle_category
             random.choice(rlce),                            # rlce
-            fake.numerify(text='#%%'),                      # fa
-            fake.lexify(text='?'),                          # trrc
-            random.choice(country),                         # registered_in_nation
+            fake.numerify(text='#%%'),                      # frontal_area
+            fake.lexify(text='?'),                          # tyre_rolling_resistance_class
+            random.choice(country),                         # registration_location
         ])
