129b85e9e4fa8a9dba3d4c3c2d3b0c29fc87c23a
Kacper S <kacpersmal@gmail.com>
2023-11-28 14:31:23 +0100

    Create UnitOfWorkTests.cs

---
+++ src/core/tests/DfT.ZEV.Core.Infrastructure.Tests/UnitOfWork/UnitOfWorkTests.cs
@@ -0,0 +1,72 @@
+using DfT.ZEV.Core.Domain.Accounts.Models;
+using DfT.ZEV.Core.Domain.Processes.Models;
+using DfT.ZEV.Core.Domain.Vehicles.Models;
+using DfT.ZEV.Core.Infrastructure.Persistence;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+
+namespace DfT.ZEV.Core.Infrastructure.Tests.UnitOfWork;
+
+[TestFixture]
+public class UnitOfWorkTests
+{
+    private AppDbContext _dbContext = null!;
+    private Infrastructure.UnitOfWork.UnitOfWork _unitOfWork = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+
+        _dbContext = new AppDbContext(options);
+        _unitOfWork = new Infrastructure.UnitOfWork.UnitOfWork(_dbContext);
+    }
+    
+    [Test]
+    public async Task Repositories_ShouldNotBeNull()
+    {
+        // Assert
+        _unitOfWork.Vehicles.Should().NotBeNull();
+        _unitOfWork.Processes.Should().NotBeNull();
+        _unitOfWork.Users.Should().NotBeNull();
+    }
+   
+
+    /* Transactions are not supported in in-memory databases.
+    
+    [Test]
+    public async Task BeginTransactionAsync_ShouldBeginTransaction()
+    {
+        // Act
+        var transactionId = await _unitOfWork.BeginTransactionAsync();
+
+        // Assert
+        transactionId.Should().NotBe(Guid.Empty);
+    }
+
+    [Test]
+    public async Task CommitTransactionAsync_ShouldCommitTransaction()
+    {
+        // Arrange
+        await _unitOfWork.BeginTransactionAsync();
+
+        // Act
+        await _unitOfWork.CommitTransactionAsync();
+
+        // Assert
+    }
+
+    [Test]
+    public async Task RollbackTransactionAsync_ShouldRollbackTransaction()
+    {
+        // Arrange
+        await _unitOfWork.BeginTransactionAsync();
+
+        // Act
+        await _unitOfWork.RollbackTransactionAsync();
+
+        // Assert
+    }*/
+}
