872f069896f9bf4b456cd2d54f5d5e9cdd4dc101
Bartosz Wojciechowski <b.wojciechowski@exlabs.pl>
2023-11-30 15:35:24 +0100

    Remove obsolete configs

--- infrastructure/terraform/output.tf
+++
@@ -1,4 +0,0 @@
-output "database_migrations_runner_url" {
-  description = "The URL of the database migrations runner function"
-  value       = google_cloudfunctions2_function.database_migrations_runner.url
-}
