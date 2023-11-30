f0c42a60a0373f342c9a832341dff139f1d73e23
Kacper S <kacpersmal@gmail.com>
2023-11-30 12:16:54 +0100

    Cleanup create user command

--- src/core/DfT.ZEV.Core.Application/Accounts/Commands/CreateUser/CreateUserCommandHandler.cs
+++ src/core/DfT.ZEV.Core.Application/Accounts/Commands/CreateUser/CreateUserCommandHandler.cs
@@ -24,19 +24,15 @@ public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, Creat
 
     public async Task<CreateUserCommandResponse> Handle(CreateUserCommand request, CancellationToken cancellationToken)
     {
-        _logger.LogInformation("Creating user with email {Email}", request.Email);
-        
-        if (await _unitOfWork.Manufacturers.GetByIdAsync(request.ManufacturerId, cancellationToken) is null)
+        var manufacturer = await _unitOfWork.Manufacturers.GetByIdAsync(request.ManufacturerId, cancellationToken);
+        if (manufacturer is null)
             throw ManufacturerHandlerExceptions.ManufacturerNotFound(request.ManufacturerId);
-        
-        var permissions = await _unitOfWork.Permissions.GetByIdsAsync(request.PermissionIds, cancellationToken);
-        permissions = permissions.ToList();
-        
+
+        var permissions = (await _unitOfWork.Permissions.GetByIdsAsync(request.PermissionIds, cancellationToken)).ToList();
         var nonExistentPermissionIds = request.PermissionIds.Except(permissions.Select(x => x.Id)).ToArray();
-        
         if (nonExistentPermissionIds.Any())
             throw UserHandlerExceptions.PermissionsNotFound(nonExistentPermissionIds);
-        
+
         var id = Guid.NewGuid();
         var args = new UserRecordArgs()
         {
@@ -49,27 +45,28 @@ public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, Creat
 
         try
         {
-            var userCreationResult = await _identityPlatform.CreateUser(args);
-        }catch(Exception e)
+            await _identityPlatform.CreateUser(args);
+        }
+        catch(Exception e)
         {
             _logger.LogError("Failed to create user: {Message}", e.Message);
             throw UserHandlerExceptions.CouldNotCreateUser(e.Message);
         }
-        
+
         var user = new User(id); 
         user.AddPermissions(permissions);
         
         await _unitOfWork.Users.InsertAsync(user, cancellationToken);
         await _unitOfWork.SaveChangesAsync(cancellationToken);
-        
+
         _logger.LogInformation("Created user with email {Email}", request.Email);
-        
+
         return new CreateUserCommandResponse
         {
             Id = user.Id
         };
     }
-    
+
     private static string CreateSecureRandomString(int count = 64) =>
         Convert.ToBase64String(RandomNumberGenerator.GetBytes(count));
 }
