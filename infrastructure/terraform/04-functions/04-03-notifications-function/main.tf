c241d51fbeb127bdd40621c21b9c10bf69c28a9e
James Cruddas <james.cruddas@informed.com>
2023-12-01 12:24:53 +0000

    [NON-JIRA] Add scaffolding for Notifications function

---
+++ infrastructure/terraform/04-functions/04-03-notifications-function/main.tf
@@ -0,0 +1,67 @@
+locals {
+  name_prefix = "${var.environment}-zev"
+  files_to_exclude_from_package = setunion(
+    fileset("../../../../src", "**/{bin,obj}/**"),
+    [".idea"]
+  )
+}
+
+data "archive_file" "notifications_service_package" {
+  type        = "zip"
+  output_path = "${path.module}/build/notifications-service-package.zip"
+  source_dir  = "../../../../src"
+  excludes    = local.files_to_exclude_from_package
+}
+
+resource "google_storage_bucket_object" "notifications_service_package" {
+  name                = "notifications-service/${data.archive_file.compliance_calculation_service_package.output_md5}-${basename(data.archive_file.notifications_service_package.output_path)}"
+  bucket              = data.terraform_remote_state.backends.outputs.cloud_function_packages_bucket_name
+  source              = data.archive_file.notifications_service_package.output_path
+  content_disposition = "attachment"
+  content_type        = "application/zip"
+}
+
+resource "google_cloudfunctions2_function" "notifications_service" {
+  name        = "${local.name_prefix}-notifications-service"
+  location    = var.region
+  description = "ZEV Notifications Service"
+
+  build_config {
+    runtime     = "dotnet6"
+    entry_point = "DfT.ZEV.Services.Notifications.Handler.Function"
+
+    environment_variables = {
+      GOOGLE_BUILDABLE = "./services/functions/notifications/DfT.ZEV.Services.Notifications.Handler"
+      BUILDID          = var.source_commit_hash
+    }
+
+    source {
+      storage_source {
+        bucket = data.terraform_remote_state.backends.outputs.cloud_function_packages_bucket_name
+        object = google_storage_bucket_object.notifications_service_package.name
+      }
+    }
+  }
+
+  service_config {
+    min_instance_count               = 0
+    max_instance_count               = var.compliance_calculation_svc_resource_quotas.max_instance_count
+    max_instance_request_concurrency = var.compliance_calculation_svc_resource_quotas.max_instance_request_concurrency
+    timeout_seconds                  = var.compliance_calculation_svc_resource_quotas.timeout_seconds
+    available_memory                 = var.compliance_calculation_svc_resource_quotas.available_memory
+    available_cpu                    = var.compliance_calculation_svc_resource_quotas.available_cpu
+
+    # TODO: Enable to limit access after configuring Cloud Tasks
+    #            ingress_settings = "ALLOW_INTERNAL_ONLY"
+    vpc_connector_egress_settings = "PRIVATE_RANGES_ONLY"
+    vpc_connector                 = data.terraform_remote_state.network.outputs.vpc_serverless_connector_id
+
+    all_traffic_on_latest_revision = true
+    service_account_email          = google_service_account.compliance_calculation_service.email
+
+    environment_variables = {
+      GoogleCloud__ProjectId = var.project
+    }
+
+  }
+}
---
+++ infrastructure/terraform/04-functions/04-03-notifications-function/variables.tf
@@ -0,0 +1,27 @@
+variable "tf_state_bucket" {
+  type        = string
+  description = "Bucket in which terraform state of other layers is held"
+}
+
+variable "region" {
+  type        = string
+  description = "GCP region to which resources will be deployed."
+  default     = "europe-west1"
+}
+
+variable "project" {
+  type        = string
+  description = "GCP project ID to which resources will be deployed."
+  nullable    = false
+}
+
+variable "environment" {
+  type        = string
+  description = "Name of the environment."
+  nullable    = false
+}
+
+variable "source_commit_hash" {
+  description = "The docker image to deploy"
+  type        = string
+}
