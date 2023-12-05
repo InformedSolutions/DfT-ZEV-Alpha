5f43fbfd91a042da28241bfe4b60a99c5b488049
cruddasj <james.cruddas@informed.com>
2023-12-05 10:01:32 +0000

    [NON-JIRA] Notification foundations (#50)
    * [NON-JIRA] Add common object for notification and csproj dependencies to notification funciton
    
    * [NON-JIRA] Add basic notifications service scaffolding
    
    * [NON-JIRA] Remove sln file
    
    * [NON-JIRA] Remove auto-added sln
    
    * [NON-JIRA] Remove secrets manager in favour of secret passed by reference

---
+++ src/common/DfT.ZEV.Common/Enumerations/NotificationType.cs
@@ -0,0 +1,10 @@
+namespace DfT.ZEV.Common.Enumerations;
+
+/// <summary>
+/// Simple enumeration to determine the type of notification to be sent.
+/// </summary>
+public enum NotificationType
+{
+    EMAIL,
+    SMS
+}
---
+++ src/common/DfT.ZEV.Common/Models/Notification.cs
@@ -0,0 +1,39 @@
+using DfT.ZEV.Common.Enumerations;
+
+namespace DfT.ZEV.Common.Models;
+
+/// <summary>
+/// Common model definition for notifications (e.g., email, SMS etc.)
+/// </summary>
+public class Notification
+{
+    /// <summary>
+    /// Gets or sets list of notification recipients.
+    /// </summary>
+    public List<string> Recipients { get; set; } = new List<string>();
+
+    /// <summary>
+    /// Gets or sets list of notification cc recipients.
+    /// </summary>
+    public List<string> Ccs { get; set; } = new List<string>();
+
+    /// <summary>
+    /// Gets or sets list of notification bcc recipients.
+    /// </summary>
+    public List<string> Bccs { get; set; } = new List<string>();
+
+    /// <summary>
+    /// Gets or sets notification template id used for rendering message body.
+    /// </summary>
+    public Guid TemplateId { get; set; }
+
+    /// <summary>
+    /// Gets or sets list of parameters used to replace template placeholders.
+    /// </summary>
+    public Dictionary<string, dynamic> TemplateParameters { get; set; } = new Dictionary<string, dynamic>();
+
+    /// <summary>
+    /// Gets or sets notification type eg. ApplicationResponse, PasswordReset etc.
+    /// </summary>
+    public NotificationType NotificationType { get; set; }
+}
