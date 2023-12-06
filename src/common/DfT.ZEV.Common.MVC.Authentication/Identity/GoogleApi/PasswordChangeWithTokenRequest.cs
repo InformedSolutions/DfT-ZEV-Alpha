bf4daaa1379ec75fa41b48bcecccd969a1a1a8af
James Cruddas <james.cruddas@informed.com>
2023-12-06 23:13:30 +0000

    [NON-JIRA] Return to use of GCP property names

--- src/common/DfT.ZEV.Common.MVC.Authentication/Identity/GoogleApi/PasswordChangeWithTokenRequest.cs
+++ src/common/DfT.ZEV.Common.MVC.Authentication/Identity/GoogleApi/PasswordChangeWithTokenRequest.cs
@@ -2,7 +2,7 @@ namespace DfT.ZEV.Common.MVC.Authentication.Identity.GoogleApi;
 
 public class PasswordChangeWithTokenRequest
 {
-  public string PasswordResetToken { get; set; }
+  public string OobCode { get; set; }
   public string NewPassword { get; set; }
   public string TenantId { get; set; }
 }
--- src/common/DfT.ZEV.Common.MVC.Authentication/Identity/Implementations/IdentityPlatform.cs
+++ src/common/DfT.ZEV.Common.MVC.Authentication/Identity/Implementations/IdentityPlatform.cs
@@ -4,16 +4,19 @@ using FirebaseAdmin;
 using FirebaseAdmin.Auth;
 using Google.Apis.Auth.OAuth2;
 using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Logging;
 
 namespace DfT.ZEV.Common.MVC.Authentication.Identity;
 
 internal sealed class IdentityPlatform : IIdentityPlatform
 {
+    private readonly ILogger<IdentityPlatform> _logger;
     private readonly IOptions<GoogleCloudConfiguration> _googleCloudConfiguration;
     private readonly IGoogleIdentityApiClient _googleIdentityApiClient;
     private readonly IOptions<ServicesConfiguration> _servicesConfiguration;
-    public IdentityPlatform(IOptions<GoogleCloudConfiguration> googleCloudConfiguration, IGoogleIdentityApiClient googleIdentityApiClient, IOptions<ServicesConfiguration> servicesConfiguration)
+    public IdentityPlatform(ILogger<IdentityPlatform> logger, IOptions<GoogleCloudConfiguration> googleCloudConfiguration, IGoogleIdentityApiClient googleIdentityApiClient, IOptions<ServicesConfiguration> servicesConfiguration)
     {
+        _logger = logger;
         _googleCloudConfiguration = googleCloudConfiguration;
         _googleIdentityApiClient = googleIdentityApiClient;
         _servicesConfiguration = servicesConfiguration;
@@ -65,9 +68,7 @@ internal sealed class IdentityPlatform : IIdentityPlatform
 
     public async Task<AuthorisationResponse> AuthenticateUser(AuthenticationRequest authorizationRequest, string tenantId)
     {
-
-        Console.WriteLine($"Authenticating user {authorizationRequest.Email} in tenant {tenantId}");
-
+        _logger.LogDebug($"Authenticating user {authorizationRequest.Email} in tenant {tenantId}");
         return await _googleIdentityApiClient.Authorise(authorizationRequest.Email, authorizationRequest.Password,
                     tenantId);
     }
@@ -76,11 +77,12 @@ internal sealed class IdentityPlatform : IIdentityPlatform
         => await _googleIdentityApiClient.RefreshToken(refreshToken);
 
 
-    public async Task ChangePasswordAsync(string oobCode, string newPassword, string tenantId)
+    public async Task ChangePasswordAsync(string passwordResetToken, string newPassword, string tenantId)
     {
+        _logger.LogDebug($"Initiating password change request using token in tenant {tenantId}");
         var request = new PasswordChangeWithTokenRequest
         {
-            PasswordResetToken = oobCode,
+            OobCode = passwordResetToken,
             NewPassword = newPassword,
             TenantId = tenantId,
         };
