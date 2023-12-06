685a63911cfd822b5d97aa764e9e3686b8e2d6cd
James Cruddas <james.cruddas@informed.com>
2023-12-06 09:35:02 +0000

    [NON-JIRA] Change class visibility

--- src/core/tests/DfT.ZEV.Core.Architecture.Tests/ApplicationTests.cs
+++ src/core/tests/DfT.ZEV.Core.Architecture.Tests/ApplicationTests.cs
@@ -19,8 +19,6 @@ public class ApplicationTests : BaseTest
             .HaveNameEndingWith("Service")
             .Should()
             .BeSealed()
-            .And()
-            .NotBePublic()
             .GetResult();
 
         result.IsSuccessful.Should().BeTrue();
