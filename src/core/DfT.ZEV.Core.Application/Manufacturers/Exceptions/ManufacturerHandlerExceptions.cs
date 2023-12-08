bc57e26081b07d62d39b63693d49c4f5682878b2
Kacper S <kacpersmal@gmail.com>
2023-12-08 16:37:22 +0100

    Add docs for exceptions

--- src/core/DfT.ZEV.Core.Application/Manufacturers/Exceptions/ManufacturerHandlerExceptions.cs
+++ src/core/DfT.ZEV.Core.Application/Manufacturers/Exceptions/ManufacturerHandlerExceptions.cs
@@ -2,11 +2,24 @@ using DfT.ZEV.Common.Exceptions;
 
 namespace DfT.ZEV.Core.Application.Manufacturers.Exceptions;
 
-public static class ManufacturerHandlerExceptions
+/// <summary>
+/// Provides methods for creating exceptions related to manufacturer handlers.
+/// </summary>
+internal static class ManufacturerHandlerExceptions
 {
-    public static HandlerException ManufacturerAlreadyExists(string name) 
+    /// <summary>
+    /// Creates a new handler exception indicating that a manufacturer already exists.
+    /// </summary>
+    /// <param name="name">The name of the manufacturer that already exists.</param>
+    /// <returns>A new handler exception.</returns>
+    public static HandlerException ManufacturerAlreadyExists(string name)
         => new($"Manufacturer with name {name} already exists");
-    
-    public static EntityNotFoundException ManufacturerNotFound(Guid id) 
+
+    /// <summary>
+    /// Creates a new entity not found exception indicating that a manufacturer was not found.
+    /// </summary>
+    /// <param name="id">The identifier of the manufacturer that was not found.</param>
+    /// <returns>A new entity not found exception.</returns>
+    public static EntityNotFoundException ManufacturerNotFound(Guid id)
         => new($"Manufacturer with id {id} not found");
 }
