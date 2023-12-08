7c647c4dfb6f5d46afcae39763a749c5cf493f18
Kacper S <kacpersmal@gmail.com>
2023-12-08 09:48:29 +0100

    Remove cycle from UserManufacturerBridge

--- src/core/DfT.ZEV.Core.Infrastructure/Persistence/Configurations/Accounts/UserConfiguration.cs
+++ src/core/DfT.ZEV.Core.Infrastructure/Persistence/Configurations/Accounts/UserConfiguration.cs
@@ -9,10 +9,12 @@ internal sealed class UserConfiguration : IEntityTypeConfiguration<User>
     public void Configure(EntityTypeBuilder<User> builder)
     {
         builder.HasKey(x => x.Id);
-        
+
         builder.Property(x => x.CreatedAt)
-            .IsRequired(); 
-        
-        
+            .IsRequired();
+
+        builder.HasMany(x => x.ManufacturerBridges)
+            .WithOne()
+            .HasForeignKey(x => x.UserId);
     }
 }
