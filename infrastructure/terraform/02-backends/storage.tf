34856908accde58039118e5b139079dbd7e3f07e
Bartosz Wojciechowski <b.wojciechowski@exlabs.pl>
2024-01-04 10:36:06 +0100

    Set up data buckets

--- infrastructure/terraform/02-backends/storage.tf
+++ infrastructure/terraform/02-backends/storage.tf
@@ -1,3 +1,4 @@
+# Application data buckets
 resource "google_storage_bucket" "manufacturer_data" {
   name     = "${local.name_prefix}-manufacturer-data"
   location = var.region
@@ -11,6 +12,46 @@ resource "google_storage_bucket" "manufacturer_data" {
   }
 }
 
+resource "google_storage_bucket" "manufacturer_portal_uploads_unscanned" {
+  name     = "${local.name_prefix}-manufacturer-portal-uploads-unscanned"
+  location = var.region
+  project  = var.project
+
+  uniform_bucket_level_access = true
+  public_access_prevention    = "enforced"
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "google_storage_bucket" "manufacturer_portal_uploads_quarantined" {
+  name     = "${local.name_prefix}-manufacturer-portal-uploads-quarantined"
+  location = var.region
+  project  = var.project
+
+  uniform_bucket_level_access = true
+  public_access_prevention    = "enforced"
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "google_storage_bucket" "manufacturer_portal_uploads_safe" {
+  name     = "${local.name_prefix}-manufacturer-portal-uploads-safe"
+  location = var.region
+  project  = var.project
+
+  uniform_bucket_level_access = true
+  public_access_prevention    = "enforced"
+
+  versioning {
+    enabled = true
+  }
+}
+
+# Utility buckets
 resource "google_storage_bucket" "cloud_function_packages" {
   name                        = "${var.environment}-cloud-functions-packages"
   location                    = var.region
